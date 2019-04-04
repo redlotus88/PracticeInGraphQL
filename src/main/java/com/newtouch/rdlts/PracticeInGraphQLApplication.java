9641fe52df738989068668a58138717b5defcc05
DragonWang <185138868@qq.com>
2019-04-04 10:16:41 +0800

    提交第一个GraphQL的实例

---
+++ src/main/java/com/newtouch/rdlts/PracticeInGraphQLApplication.java
@@ -0,0 +1,15 @@
+package com.newtouch.rdlts;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+
+/**
+ * Created by dragon on 2019/4/3.
+ */
+@SpringBootApplication
+public class PracticeInGraphQLApplication {
+
+    public static void main(String[] args) {
+        new SpringApplicationBuilder(PracticeInGraphQLApplication.class).run(args);
+    }
+}
